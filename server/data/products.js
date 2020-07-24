93a34a691ac29aee113e0c23f528b9b828664c64
Irfan <maulana.irfan@tokopedia.com>
2020-07-24 18:50:15 +0700

    feat: basic data

---
+++ server/data/products.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    name: 'Samsung Protective Standing Cover S10 - Black',
+    product_url: 'https://www.tokopedia.com/samsung/samsung-protective-standing-cover-s10-black',
+    product_id: '1234567890',
+    price: 'Rp 499.000',
+    images: '',
+    stats: {
+      reviewCount: 3,
+      rating: 93,
+    },
+  },
+];
