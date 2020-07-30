74833ba2c3e06144a7ded81424a242be3eaa4965
Joshua Stephen <13672022+josteph@users.noreply.github.com>
2020-07-30 11:32:55 +0700

    feat: move disqus to app level code (#9)

--- server/renderer/thirdparty.js
+++ server/renderer/thirdparty.js
@@ -11,26 +11,3 @@ export const hotjarScript =  `
 			})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 	</script>
 `;
-
-export const disqusScript =  `
-	<div id="disqus_thread" style="max-width: 500px;margin: 0 auto;padding: 2em;border-left: 1px solid rgb(255, 255, 255);border-right: 1px solid rgb(255, 255, 255);"></div>
-	<script>
-		/**
-		*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-		*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-		/*
-		var disqus_config = function () {
-			this.page.url = 'https://tkpd-ta-web-vitals.glitch.me/';  // Replace PAGE_URL with your page's canonical URL variable
-			this.page.identifier = 'home'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-		};
-		*/
-		(function() { // DON'T EDIT BELOW THIS LINE
-			var d = document, s = d.createElement('script');
-			s.src = 'https://test-disqus-13.disqus.com/embed.js';
-			s.setAttribute('data-timestamp', +new Date());
-			(d.head || d.body).appendChild(s);
-		})();
-	</script>
-	<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-	<script id="dsq-count-scr" src="//test-disqus-13.disqus.com/count.js" async></script>
-`;
